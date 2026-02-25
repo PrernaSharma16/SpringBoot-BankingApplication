b42e53b6a3fdaf1bfec510b3cf5720102f8a3049
prerna sharma <prerna.sharma160702@gmail.com>
2026-02-25 16:11:51 +0530

    Implemented Swagger OpenAPI for professional API testing

--- BankingSystem/src/main/java/com/banking/controller/CustomerController.java
+++ BankingSystem/src/main/java/com/banking/controller/CustomerController.java
@@ -17,14 +17,21 @@ import com.banking.entity.Transaction;
 import com.banking.repository.TransactionRepository;
 import com.banking.service.CustomerService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 /*@RestController: marks class as REST API controller. Returns JSON directly
   @RequestMapping("/API/customers"): Base URL for all end points
   @PostMapping: Handles HTTP POST requests
   @RequestBody: Converts JSON into java object automatically
-  @PathVariable: extracts value from URL*/
+  @PathVariable: extracts value from URL
+  @Tag: Used on controller classes to group API end points and provide descriptions in the generated doc 
+  (In case of tools like Swagger) 
+  @Operation: Used to provide detailed metadata for a specific API in a REST controller*/
 
+@Tag(name = "Customer APIs", description = "Operations related to customers and transactions")
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
@@ -70,17 +77,25 @@ public class CustomerController {
 		return ResponseEntity.ok(customerService.withdraw(id, amount));
 	}
 	
+	@Operation(summary = "Get paginated transaction history of a customer")
 	@GetMapping("/transactions/{id}")
+	
 	public ResponseEntity<Page<TransactionResponseDTO>> getTransactions(
-			@PathVariable Long id,
+			@Parameter (description = "Customer ID") @PathVariable Long id,
+			@Parameter (description = "Page number (starts from 0)")
 			@RequestParam(defaultValue = "0") int page,
+			@Parameter(description = "Number of records per page")
 			@RequestParam(defaultValue = "5") int size,
+			@Parameter(description = "Field to sort by")
 			@RequestParam(defaultValue = "transactionTime") String sortBy,
+			@Parameter(description = "Sorting direction: asc or desc")
 			@RequestParam(defaultValue = "desc") String direction){
 		
 		return ResponseEntity.ok(customerService.getTransactionHistory(id, page, size, sortBy, direction));
 	}
 	
+	@Operation(summary = "Transfer money between two customers",
+			description = "Transfers specified amount from sender to receiver and creates transaction records for both")
 	@PostMapping("/transfer")
 	public ResponseEntity<TransferResponseDTO> transfer(@Valid @RequestBody TransferRequestDTO request){
 		return ResponseEntity.ok(customerService.transfer(request));
--- BankingSystem/src/main/java/com/banking/dto/TransferRequestDTO.java
+++ BankingSystem/src/main/java/com/banking/dto/TransferRequestDTO.java
@@ -1,18 +1,23 @@
 package com.banking.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+@Schema(description = "Request object for transferring money between customers")
 public class TransferRequestDTO {
 	
 	@NotNull(message = "Sender ID is required")
+	@Schema(description = "Sender Customer ID", example="1")
 	private Long senderId;
 	
+	@Schema(description = "Receiver Customer ID", example = "2")
 	@NotNull(message = "Receiver ID is required")
 	private Long receiverId;
 	
 	@NotNull(message = "Amount is required")
 	@Positive(message = "Amount must be positive")
+	@Schema(description = "Amount to transfer", example = "500.0")
 	private Double amount;
 
 	public Long getSenderId() {
