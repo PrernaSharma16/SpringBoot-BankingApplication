aa53658da104a04725f556b5d829ebc0548077f9
prerna sharma <prerna.sharma160702@gmail.com>
2026-03-05 11:50:57 +0530

    Resolved the bugs in JWT Authentication

--- BankingSystem/src/main/java/com/banking/controller/AuthController.java
+++ BankingSystem/src/main/java/com/banking/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.banking.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +23,17 @@ public class AuthController {
 	@Autowired
 	private JwtService jwtService;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@PostMapping("/login")
 	public String login(@RequestBody AuthRequest request) {
 		
 		authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
 		
-		return jwtService.generateToken(request.getUsername());
+		UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+		
+		return jwtService.generateToken(userDetails);
 	}
 }
--- BankingSystem/src/main/java/com/banking/security/JwtService.java
+++ BankingSystem/src/main/java/com/banking/security/JwtService.java
@@ -2,7 +2,11 @@ package com.banking.security;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
@@ -18,9 +22,18 @@ public class JwtService {
 		return Keys.hmacShaKeyFor(SECRET.getBytes());
 	}
 	
-	public String generateToken(String username) {
+	public String generateToken(UserDetails userDetails) {
+		
+		Map<String, Object> claims = new HashMap<>();
+		
+		claims.put("roles", userDetails.getAuthorities()
+				.stream()
+				.map(GrantedAuthority::getAuthority)
+				.toList());
+		
 		return Jwts.builder()
-				.setSubject(username)
+				.setClaims(claims)
+				.setSubject(userDetails.getUsername())
 				.setIssuedAt(new Date())
 				.setExpiration(new Date(System.currentTimeMillis() + 1000*60*60)) //1 hour
 				.signWith(getSignKey(), SignatureAlgorithm.HS256)
